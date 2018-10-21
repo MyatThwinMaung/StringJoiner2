6cdc14a20354e2c4669d9672ddbe2cbd8b27ae84
MMRNB0038\techfunmmr <ycc.myatthwinmaung@gmail.com>
2018-10-21 13:45:30 +0630

    added setEmptyValue

--- Lecture3/src/TestStringJoiner.java
+++ Lecture3/src/TestStringJoiner.java
@@ -4,11 +4,31 @@ public class TestStringJoiner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		testStringJoiner();
+		testWithSetEmptyValue();
+		testBothEmptyAndAdd();
+	}
+
+	private static void testStringJoiner() {
 		StringJoiner sj = new StringJoiner("], [", "[", "]");
 		sj.add("Myat").add("Thwin").add("Maung");
 		String result = sj.toString();
 		System.out.println(result);
-		System.out.println("haha");
 	}
 
+	private static void testWithSetEmptyValue() {
+		StringJoiner sj = new StringJoiner(", ", "{", "}");
+		sj.setEmptyValue("Empty String");
+		sj.toString();
+		System.out.println(sj);
+	}
+
+	private static void testBothEmptyAndAdd() {
+		StringJoiner sj = new StringJoiner(",", "[", "]");
+		sj.setEmptyValue("Empty String");
+		sj.add("");
+		sj.toString();
+		System.out.println(sj);
+	}
 }
